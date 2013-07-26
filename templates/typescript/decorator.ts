b29a07d41d8beb8c718832affce27abfe7b43207
SpeedShifter <speeedermak@gmail.com>
2013-07-26 11:11:32 +0300

    Decorator TS

--- templates/typescript/decorator.ts
+++ templates/typescript/decorator.ts
@@ -1,9 +1,15 @@
+/// <reference path="../../bower_components/dt-angular/angular.d.ts" />
 'use strict';
 
-angular.module('<%= _.camelize(appname) %>App')
-    .config(function ($provide) {
-        $provide.decorator('<%= _.camelize(name) %>', function ($delegate) {
+module <%= _.camelize(appname) %>.Decorators {
+    class <%= _.classify(name) %> {
+        private static $inject = ['$delegate'];
+        constructor($delegate){
             // decorate the $delegate
             return $delegate;
-        });
-    });
+        }
+    }
+    app.config(function ($provide: ng.IProvide) {
+        $provide.decorator('<%= _.camelize(appname) %>.Decorators.<%= _.camelize(name) %>', <%= _.classify(name) %>);
+    }
+}
